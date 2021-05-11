5d8f1a8435d3f9435dbb70ee5ae235f44220db8c
Sean Lawlor <lawlorseanr@gmail.com>
2021-05-11 17:23:57 -0400

    Implement recursive multiply w/ mixed number support

--- src/recursion.js
+++ src/recursion.js
@@ -203,6 +203,30 @@ var modulo = function(x, y) {
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+
+  if (x === 0 || y === 0) {
+    return 0;
+  }
+
+  if (y === 1 || y === -1) {
+    return x;
+  }
+
+  if (x < 0 && y < 0) {
+    x = -x;
+    y = -y;
+  }
+
+  if (x < 0 && y > 0) {
+    return x - multiply(-x, y - 1);
+  }
+
+  if (x > 0 && y < 0) {
+    return -x - multiply(x, -y - 1)
+  }
+
+  return x + multiply(x, y - 1);
+
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
