abb677d9613a87c0a02c769aa8e354edc5d55846
Sean Lawlor <lawlorseanr@gmail.com>
2021-05-11 12:33:03 -0400

    Implement recursive exponent, optimized for positive numbers

--- src/recursion.js
+++ src/recursion.js
@@ -106,6 +106,20 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+
+  if (exp === 0) {
+    return 1;
+  }
+
+  if (exp === 2) {
+    return base * base;
+  }
+
+  if (exp < 0) {
+    return 1 / (base * exponent(base, -exp - 1));
+  }
+
+  return base * exponent(base, exp - 1);
 };
 
 // 8. Determine if a number is a power of two.
