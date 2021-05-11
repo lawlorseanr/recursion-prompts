07cabc55fa5804a92d9331c72167374ddf280d06
Sean Lawlor <lawlorseanr@gmail.com>
2021-05-11 12:50:43 -0400

    Implement recursive palindrome check

--- src/recursion.js
+++ src/recursion.js
@@ -148,8 +148,19 @@ var reverse = function(string) {
 
 };
 
-// 10. Write a function that determines if a string is a palindrome.
+// 10. Write a function that determines if a string is a palindrome
 var palindrome = function(string) {
+
+  if (string.length <= 1) {
+    return true;
+  }
+
+  if (string[0].toLowerCase() !== string[string.length -1].toLowerCase()) {
+    return false;
+  }
+
+  return true && palindrome(string.slice(1,string.length - 1));
+
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
